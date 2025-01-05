f8e7b5ea968f8d7e32b05f6734a3524e49cf7ee9
muzammil <ismailmuzammil206@gmail.com>
2025-01-05 16:30:26 +0100

    updated

--- app/map/page.js
+++ app/map/page.js
@@ -5,12 +5,12 @@ export default function SatelliteDataVisualization() {
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 bg-gray-100">
-        {/* Map placeholder */}
+        
         <div 
           className="w-full h-full bg-cover bg-center"
           style={{backgroundImage: "url('/placeholder.svg?height=1080&width=1920')"}}
         >
-          {/* You can replace this with your actual map component later */}
+          
         </div>
       </div>
     </div>
