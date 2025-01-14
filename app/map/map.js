41bab20fa68c8ed02a441a1465826a3694f17684
muzammil <ismailmuzammil206@gmail.com>
2025-01-14 02:40:40 +0100

    added sample imagery

--- app/map/map.js
+++ app/map/map.js
@@ -3,6 +3,7 @@
 import React, { useRef, useEffect } from "react";
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
+import ImageryLayer from "@arcgis/core/layers/ImageryLayer";
 import "@arcgis/core/assets/esri/themes/light/main.css"; 
 
 const EsriMap = () => {
@@ -16,14 +17,31 @@ const EsriMap = () => {
         basemap: "topo-vector", 
       });
 
+      const layer = new ImageryLayer({
+        url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/NLCDLandCover2001/ImageServer",
+        format: "jpgpng" // server exports in either jpg or png format
+      });
+
+
+      const map = new Map({
+        basemap: "gray-vector",
+        layers: [layer]
+      });
+
       view = new MapView({
         container: mapRef.current,
-        map: baseMap,
-        center: [-118.805, 34.027], 
+        map: map,
+        center: [-100, 40], 
         zoom: 10, 
       });
-    }
 
+
+     
+     
+    
+
+    }
+   
     return () => {
       if (view) {
         view.destroy();
